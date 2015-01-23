8e25a70dbe903c3708fddb5005e5ae03cdc93cbd
Bradley Heinz <bradley.heinz@gmail.com>
2015-01-22 16:36:30 -0800

    fixed some shit, like what's in each named filed
    \f0\fs24 \cf0 hello world}

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -1,72 +1,7 @@
-    // this is what you would do if you liked things to be easy:
-    // var stringifyJSON = JSON.stringify;
+// this is what you would do if you were one to do things the easy way:
+// var parseJSON = JSON.parse;
 
-    // but you don't so you're going to write it from scratch:
-
-    var stringifyJSON = function(obj) {
-      // sortingHat(obj);
-      // your code goes here
-      return stringify(obj);
-    };
-
-    var sortingHat = function(input) {
-        switch (typeof(input)){
-            case 'object':
-                return objectifier(input);
-                break;
-            case 'number':
-                return input.toString();
-                break;
-            case 'string':
-                return '\"' + input + '\"';
-                break;
-            case 'boolean':
-                return input.toString();
-                break;
-            case 'undefined':
-                return null;
-                break;
-            default:
-                console.log('Huh? Well this was unexpected.');
-        }
-    };
-
-    var objectifier = function(object){
-        if (object === null){
-        console.log("That shit is null");
-        return 'null';
-        }
-        else if (Array.isArray(object)) {
-            console.log("That's an array, baby!")
-            return arrayStringify(object);
-        }
-        else {
-            console.log("That's an object. Let's do something with it.")
-        }
-    };
-
-    var arrayStringify = function(array){
-        var arrayHolder = '[';
-        if (array.length === 0){
-            arrayHolder += ']';
-            return arrayHolder
-        }
-        else if (array.length === 1) {
-            arrayHolder += sortingHat(array[0]);
-            arrayHolder += ']';
-            return arrayHolder
-        }
-        else {
-            for (var i = 0; i < array.length - 1; i++) {
-                arrayHolder += sortingHat(array[i]);
-                arrayHolder += ",";
-            }
-            var lastElement = array.pop();
-            arrayHolder += sortingHat(lastElement);
-            arrayHolder += ']';
-            return arrayHolder;
-        }
-    };
-
-        console.log(sortingHat([5]));
-
+// but you're not, so you'll write it from scratch:
+var parseJSON = function(json) {
+  // your code goes here
+};
